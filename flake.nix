6700d30b9fefee37b63fc841a550222bef3384ef
Talha Abdulkuddus <git@talhaak.com>
2025-07-29 16:42:39 +0100

    chore(nixos): update flake.lock + remove dep to old dotfiles

--- flake.nix
+++ flake.nix
@@ -2,18 +2,13 @@
   description = "Nix Configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    dots = {
-      url = "github:Talha-Ak/nvim-config";
-      flake = false;
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
+    self,
     nixpkgs,
     home-manager,
     ...
@@ -21,12 +16,10 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    nixosConfigurations = {
-      caelid = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./nixos/configuration.nix
-        ];
-      };
+    nixosConfigurations.caelid = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./nixos/configuration.nix
+      ];
     };
 
     homeConfigurations = {
